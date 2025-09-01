34e57e3c6162347e5ea266521eeb07161f79721a
Germán Aliprandi <galiprandi@gmail.com>
2025-09-01 19:18:33 -0300

    feat: add offers table and update receptions table format with markdown and quick replies

--- app/prompts/modules.prompt.ts
+++ app/prompts/modules.prompt.ts
@@ -2,13 +2,37 @@ export const modulesPrompt = `
 🚚 Recepciones:
 - Cuando debas mostran las recepciones de productos, busca en los datos del usuario a que store (tienda) pertenece.
 - Pinta una tabla con las recepciones de productos.
+- No incluyas el código EAN en la tabla.
+- Incluye los siguientes datos del producto: nombre, categoría, cantidad.
+
 
 Ejemplo de la tabla:
-🚚 [storeName] | [id] | hora estimada de llegada: [estimatedTime]
+\`\`\`markdown
+🚚 Envio: [id] (hora estimada de llegada: [estimatedTime])
+
+---
 
 | Producto | Categoría | Cantidad |
 |---|---|---|
 | Producto 1 | Categoría 1 | 5 |
 | Producto 2 | Categoría 2 | 3 |
+
+\`\`\`
+QUICK_REPLIES: 📧 Al email, 📲 Whatsapp, 🖨️ Imprimir.
+
+
+🎁 Ver ofertas:
+- Pinta una tablacon los productos en oferta incluyendo: image, name, category, price.
+
+Ejemplo de la tabla:
+\`\`\`markdown
+|  | Producto | Categoría | Precio | 🔗 |
+|---|---|---|---|---|
+| ![name](image) | Producto 1 | Categoría 1 | $9.99 | [link](url) |
+| ![name](image) | Producto 2 | Categoría 2 | $19.99 | [link](url) |
+
+\`\`\`
+QUICK_REPLIES: 📧 Al email, 📲 Whatsapp, 🖨️ Imprimir.
+
 
 `;
