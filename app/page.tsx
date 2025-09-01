ab58a30d7b3e4e83fbf97c98bf296576f090ee84
Germán Aliprandi <galiprandi@gmail.com>
2025-08-31 22:03:12 -0300

    style: add padding and margin to chat container to prevent scrollbar overlap

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,10 @@ export default function Chat() {
 
   return (
     <div className="flex flex-col w-full max-w-[1120px] mx-auto h-screen px-[1em] py-[1em]">
-      <div className="flex-1 overflow-y-auto">
+      <div
+        className="flex-1 overflow-y-auto"
+        style={{ paddingRight: "1em", marginRight: "-1em" }}
+      >
         {messages.map((message) => (
           <div key={message.id} className="whitespace-pre-wrap">
             {message.parts.map((part, i) => {
